96b4d23da86267a0af0e1c8cd9938c031f23db57
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-05-23 10:58:58 +0200

    add ghost monster type

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.actors.Ghost;
 import com.codecool.dungeoncrawl.data.actors.Player;
 import com.codecool.dungeoncrawl.data.actors.Skeleton;
 import com.codecool.dungeoncrawl.data.objects.Key;
@@ -39,6 +40,10 @@ public class MapLoader {
                         case 's':
                             cell.setType(CellType.FLOOR);
                             new Skeleton(cell);
+                            break;
+                        case 'g':
+                            cell.setType(CellType.FLOOR);
+                            new Ghost(cell);
                             break;
                         case '@':
                             cell.setType(CellType.FLOOR);
--- src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
@@ -30,6 +30,7 @@ public class Tiles {
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("sword", new Tile(0,29));
         tileMap.put("key", new Tile(16,23));
+        tileMap.put("ghost", new Tile(26,6));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
